1e7a750d04638a7db06269a26e95a087da16b4ae
csoltero109 <chrissoltero109@gmail.com>
2018-12-06 17:01:17 -0800

    Fixed login issues and added logout item

--- app/src/main/java/com/example/dkale/wooby/MainActivity.java
+++ app/src/main/java/com/example/dkale/wooby/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,45 +28,89 @@ import com.google.firebase.database.ValueEventListener;
 import static com.google.firebase.auth.FirebaseAuth.getInstance;
 
 public class MainActivity extends AppCompatActivity {
+    final String TAG = "FirebaseTest";
 
     FirebaseApp mApp;
     FirebaseAuth mAuth;
+
     FirebaseAuth.AuthStateListener mAuthListener;
-    final String TAG = "Firebase Status";
-    String mDisplayname;
+    String mDisplayName;
+    TextView mScreenMessage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         initFirebase();
     }
 
-    private void initFirebase(){
+    private void initFirebase() {
+
         mApp = FirebaseApp.getInstance();
         mAuth = FirebaseAuth.getInstance(mApp);
+
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = mAuth.getCurrentUser();
-                if(user != null){
-                    Log.e(TAG,"User is valid. Email: " + user.getEmail() + " Display Name: " + user.getDisplayName());
 
-                    String displayName = user.getDisplayName();
-                    if(displayName != null){
-                        mDisplayname = displayName;
-                    }
-                    else{
-                        mDisplayname = "Unknown Display Name";
-                    }
-                }
-                else{
-                    Log.e(TAG,"There is no valid user logged on");
-                    mDisplayname = "Null";
-                    Intent signIn = new Intent(getApplicationContext(), SignIn.class);
-                    startActivity(signIn);
+                FirebaseUser user = mAuth.getCurrentUser();
+                if (user != null) {
+                    Log.e(TAG,"AUTH STATE UPDATE : Valid user logged in [" + user.getEmail() + "] [" + user.getDisplayName() + "]" );
+
+//                    String displayName = user.getDisplayName().toString();
+//                    mScreenMessage.setText("User : " + displayName);
+
+//                    if (displayName != null)
+//                        mDisplayName = displayName;
+//                    else
+//                        mDisplayName = "Unknown DisplayName";
+                } else {
+                    Log.e(TAG,"AUTH STATE UPDATE : NO valid user logged in");
+                    mDisplayName = "No Valid User";
+
+                    mAuth.removeAuthStateListener(mAuthListener);
+                    Intent signInIntent = new Intent(getApplicationContext(), SignIn.class);
+                    startActivityForResult(signInIntent, 101);
                 }
             }
         };
         mAuth.addAuthStateListener(mAuthListener);
+
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        Log.e(TAG,"Activity returned");
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 101) {
+
+                mDisplayName = data.getStringExtra("displayname");
+                Log.e(TAG,"Intent returned Display Name [" + mDisplayName + "]");
+
+                mAuth.addAuthStateListener(mAuthListener);
+            }
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.menu_logout) {
+            Log.e(TAG,"Logout selected");
+
+            mAuth.signOut();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
