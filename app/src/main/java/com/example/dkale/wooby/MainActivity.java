140525bf6e3b2e1e7d81c7c597e4a414e40957b0
Unknown <lefeng96@gmail.com>
2018-12-09 19:04:11 -0800

    graphql and main changes

--- app/src/main/java/com/example/dkale/wooby/MainActivity.java
+++ app/src/main/java/com/example/dkale/wooby/MainActivity.java
@@ -64,8 +64,8 @@ public class MainActivity extends AppCompatActivity implements ChatMessageFragme
         initFirebase();
         initViewPager();
 //        initDatabase();
-//        aniList = (TextView) findViewById(R.id.anilist);
-//        apolloTest();
+        aniList = (TextView) findViewById(R.id.anilist);
+        apolloTest();
     }
 
     private void initFirebase() {
@@ -201,41 +201,42 @@ public class MainActivity extends AppCompatActivity implements ChatMessageFragme
         ref.addValueEventListener(listener);
     }
 
-//    private TextView aniList;
-//    final static String BASE_URL = "https://graphql.anilist.co";
+    private TextView aniList;
+    final static String BASE_URL = "https://graphql.anilist.co";
 
 
-//    public void apolloTest(){
-//
-//        OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
-//        ApolloClient apolloClient = ApolloClient.builder().serverUrl(BASE_URL).okHttpClient(okHttpClient).build();
-//        final TestQuery testQuery = TestQuery.builder().genre(new ArrayList<String>(Arrays.asList("hentai"))).score(60).sort(new ArrayList<MediaSort>(Arrays.asList(MediaSort.SCORE_DESC))).build();
-//        apolloClient.query(testQuery).enqueue(new ApolloCall.Callback<TestQuery.Data>() {
-//            @Override
-//            public void onResponse(@NotNull Response<TestQuery.Data> response) {
-//                final StringBuffer buffer = new StringBuffer();
-//                TestQuery.Data anime = response.data();
-//                for(int x = 0; x < anime.Page().media().size(); x++){
-//                    buffer.append("id: " + anime.Page().media().get(x).id());
-//                    buffer.append("title: " + anime.Page().media().get(x).title());
-//                    buffer.append("averageScore: " + anime.Page().media().get(x).averageScore());
-//                    buffer.append("\n~~~~~~~~~~~");
-//                    buffer.append("\n\n");
-//                }
-//
-//                MainActivity.this.runOnUiThread(new Runnable() {
-//                    @Override public void run() {
-//                        TextView txtResponse = aniList;
-//                        txtResponse.setText(buffer.toString());
-//                    }
-//                });
-//
-//            }
-//
-//            @Override
-//            public void onFailure(@NotNull ApolloException e) {
-//
-//            }
-//        });
-//    }
+    public void apolloTest(){
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
+        ApolloClient apolloClient = ApolloClient.builder().serverUrl(BASE_URL).okHttpClient(okHttpClient).build();
+        final TestQuery testQuery = TestQuery.builder().genre(new ArrayList<String>(Arrays.asList("hentai"))).score(60).sort(new ArrayList<MediaSort>(Arrays.asList(MediaSort.SCORE_DESC))).build();
+        apolloClient.query(testQuery).enqueue(new ApolloCall.Callback<TestQuery.Data>() {
+            @Override
+            public void onResponse(@NotNull Response<TestQuery.Data> response) {
+                final StringBuffer buffer = new StringBuffer();
+                TestQuery.Data anime = response.data();
+                for(int x = 0; x < anime.Page().media().size(); x++){
+                    buffer.append("title: " + anime.Page().media().get(x).title());
+                    buffer.append("averageScore: " + anime.Page().media().get(x).averageScore());
+                    buffer.append("coverImage: " + anime.Page().media().get(x).coverImage());
+                    buffer.append("genres: " + anime.Page().media().get(x).genres());
+                    buffer.append("\n~~~~~~~~~~~");
+                    buffer.append("\n\n");
+                }
+
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override public void run() {
+                        TextView txtResponse = aniList;
+                        txtResponse.setText(buffer.toString());
+                    }
+                });
+
+            }
+
+            @Override
+            public void onFailure(@NotNull ApolloException e) {
+
+            }
+        });
+    }
 }
