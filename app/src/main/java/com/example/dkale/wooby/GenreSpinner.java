2bf9c975d2395649752dd0d9c8759dacf0077185
KagamiToastSlap <alvint777@yahoo.com>
2018-12-10 20:48:21 -0800

    commented out apollo things

--- app/src/main/java/com/example/dkale/wooby/GenreSpinner.java
+++ app/src/main/java/com/example/dkale/wooby/GenreSpinner.java
@@ -1,25 +1,25 @@
-package com.example.dkale.wooby;
-
-import android.app.Activity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-
-public class GenreSpinner extends Activity implements AdapterView.OnItemSelectedListener {
-
-//    Spinner spinner = (Spinner) findViewById(R.id.genre_spinner);
-//    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.ani_genres,android.R.layout.simple_spinner_item);
-//     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        spinner.setAdapter(adapter);
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
-}
+//package com.example.dkale.wooby;
+//
+//import android.app.Activity;
+//import android.view.View;
+//import android.widget.AdapterView;
+//import android.widget.ArrayAdapter;
+//import android.widget.Spinner;
+//
+//public class GenreSpinner extends Activity implements AdapterView.OnItemSelectedListener {
+//
+////    Spinner spinner = (Spinner) findViewById(R.id.genre_spinner);
+////    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.ani_genres,android.R.layout.simple_spinner_item);
+////     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+////        spinner.setAdapter(adapter);
+//
+//    @Override
+//    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//
+//    }
+//
+//    @Override
+//    public void onNothingSelected(AdapterView<?> parent) {
+//
+//    }
+//}
--- app/src/main/java/com/example/dkale/wooby/MainActivity.java
+++ app/src/main/java/com/example/dkale/wooby/MainActivity.java
@@ -61,15 +61,15 @@ public class MainActivity extends AppCompatActivity implements ChatMessageFragme
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setContentView(R.layout.fragment_suggestion);
+    //    setContentView(R.layout.fragment_suggestion);
         initFirebase();
         initViewPager();
 //        initDatabase();
-        aniName = (TextView) findViewById(R.id.aniName);
-        aniGenre = (TextView) findViewById(R.id.aniGenre);
-        aniRating = (TextView) findViewById(R.id.aniRating);
-        imageView = (ImageView) findViewById(R.id.imageView);
-        apolloTest();
+//        aniName = (TextView) findViewById(R.id.aniName);
+//        aniGenre = (TextView) findViewById(R.id.aniGenre);
+//        aniRating = (TextView) findViewById(R.id.aniRating);
+//        imageView = (ImageView) findViewById(R.id.imageView);
+//        apolloTest();
     }
 
     private void initFirebase() {
@@ -212,44 +212,44 @@ public class MainActivity extends AppCompatActivity implements ChatMessageFragme
     final static String BASE_URL = "https://graphql.anilist.co";
 
 
-    public void apolloTest(){
-
-        OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
-        ApolloClient apolloClient = ApolloClient.builder().serverUrl(BASE_URL).okHttpClient(okHttpClient).build();
-        final TestQuery testQuery = TestQuery.builder().genre(new ArrayList<String>(Arrays.asList("hentai"))).score(60).sort(new ArrayList<MediaSort>(Arrays.asList(MediaSort.SCORE_DESC))).build();
-        apolloClient.query(testQuery).enqueue(new ApolloCall.Callback<TestQuery.Data>() {
-            @Override
-            public void onResponse(@NotNull Response<TestQuery.Data> response) {
-                final StringBuffer title = new StringBuffer();
-                final StringBuffer genre = new StringBuffer();
-                final StringBuffer rating = new StringBuffer();
-                final StringBuffer img = new StringBuffer();
-                TestQuery.Data anime = response.data();
-                for(int x = 0; x < anime.Page().media().size(); x++){
-                    title.append("title: " + anime.Page().media().get(x).title());
-                    genre.append("averageScore: " + anime.Page().media().get(x).averageScore());
-                    rating.append("coverImage: " + anime.Page().media().get(x).coverImage());
-                    img.append("genres: " + anime.Page().media().get(x).genres());
-                }
-
-                MainActivity.this.runOnUiThread(new Runnable() {
-                    @Override public void run() {
-                        TextView titleRes = aniName;
-                        TextView genreRes = aniGenre;
-                        TextView ratingRes = aniRating;
-                        ImageView imgRes = imageView;
-                        titleRes.setText(title.toString());
-                        genreRes.setText(genre.toString());
-                        ratingRes.setText(rating.toString());
-                    }
-                });
-
-            }
-
-            @Override
-            public void onFailure(@NotNull ApolloException e) {
-
-            }
-        });
-    }
+//    public void apolloTest(){
+//
+//        OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
+//        ApolloClient apolloClient = ApolloClient.builder().serverUrl(BASE_URL).okHttpClient(okHttpClient).build();
+//        final TestQuery testQuery = TestQuery.builder().genre(new ArrayList<String>(Arrays.asList("hentai"))).score(60).sort(new ArrayList<MediaSort>(Arrays.asList(MediaSort.SCORE_DESC))).build();
+//        apolloClient.query(testQuery).enqueue(new ApolloCall.Callback<TestQuery.Data>() {
+//            @Override
+//            public void onResponse(@NotNull Response<TestQuery.Data> response) {
+//                final StringBuffer title = new StringBuffer();
+//                final StringBuffer genre = new StringBuffer();
+//                final StringBuffer rating = new StringBuffer();
+//                final StringBuffer img = new StringBuffer();
+//                TestQuery.Data anime = response.data();
+//                for(int x = 0; x < anime.Page().media().size(); x++){
+//                    title.append("title: " + anime.Page().media().get(x).title());
+//                    genre.append("averageScore: " + anime.Page().media().get(x).averageScore());
+//                    rating.append("coverImage: " + anime.Page().media().get(x).coverImage());
+//                    img.append("genres: " + anime.Page().media().get(x).genres());
+//                }
+//
+//                MainActivity.this.runOnUiThread(new Runnable() {
+//                    @Override public void run() {
+//                        TextView titleRes = aniName;
+//                        TextView genreRes = aniGenre;
+//                        TextView ratingRes = aniRating;
+//                        ImageView imgRes = imageView;
+//                        titleRes.setText(title.toString());
+//                        genreRes.setText(genre.toString());
+//                        ratingRes.setText(rating.toString());
+//                    }
+//                });
+//
+//            }
+//
+//            @Override
+//            public void onFailure(@NotNull ApolloException e) {
+//
+//            }
+//        });
+//    }
 }
