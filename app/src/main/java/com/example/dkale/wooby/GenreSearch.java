62d44a087b7e7f6bcff92e4eef763e5d8464f5c7
Daniel Kale <dkalechip@gmail.com>
2018-12-12 14:12:13 -0800

    Pushing graphql changes

--- app/src/main/java/com/example/dkale/wooby/GenreSearch.java
+++ app/src/main/java/com/example/dkale/wooby/GenreSearch.java
@@ -1,4 +1,22 @@
 package com.example.dkale.wooby;
 
+import android.os.Bundle;
+
 public class GenreSearch {
+    private String[] genreList = {"action", "adventure","comedy","drama","ecchi",
+            "fantasy","horror", "mahou shoujo","mecha","music","mystery","psychological",
+            "romance","sci-fi","slice of life","sports","supernatural","thriller"};
+
+    private String genreQuery = "";
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.genre_search);
+    }
+
+
 }
+
--- app/src/main/java/com/example/dkale/wooby/GenreSearchAdapter.java
+++ app/src/main/java/com/example/dkale/wooby/GenreSearchAdapter.java
@@ -1,4 +1,6 @@
 package com.example.dkale.wooby;
 
 public class GenreSearchAdapter {
+
+
 }
